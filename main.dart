ea400c09c3efffb586cf97db905db3a673d2b243
asaqa988 <asaqa001@gmail.com>
2022-08-25 17:35:01 +0300

    updated for the second time abedalraheem

--- main.dart
+++ main.dart
@@ -1,5 +1,21 @@
-void main(){
 
-  String myname = "abedalraheem";
+void main() {
+  Map mohamamdInformation = {
+    "first_name": "mohammad",
+    "last_name": "omar",
+    "age": 24,
+    "familyMemebers":[{
+      "father":"omar",
+    "Mother":"dana",
+    "sister":"rawan","brothers":["ali","anas"]}],
+    "education info":{
+      "name":"univeristy of jordan",
+      "faculty":"IT",
+      "year":2022
+    }
+
+  };
 
 }
+
+
